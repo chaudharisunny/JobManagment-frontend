c6a5a07a75dd598fc60260186e6b2a8349541a8a
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-06 11:55:07 +0530

    update code

--- src/Admin/pages/AllUsers.jsx
+++ src/Admin/pages/AllUsers.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../../services/api";
+import API from "../../services/Api.js";
 
 const AllUsers = () => {
   const navigate = useNavigate();

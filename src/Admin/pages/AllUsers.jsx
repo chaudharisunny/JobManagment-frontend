7eba756ecc0ffb1839d7799042c5cb6cc1b1cdb7
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-06 12:23:04 +0530

    fix: alias + api imports + env

--- src/Admin/pages/AllUsers.jsx
+++ src/Admin/pages/AllUsers.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API from "../../services/Api.js";
+import API from "@/services/api";
 
 const AllUsers = () => {
   const navigate = useNavigate();

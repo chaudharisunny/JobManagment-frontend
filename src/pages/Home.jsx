b57d0498a7189a849ee64a35947e724dbe0213bc
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-07 10:45:24 +0530

    fix imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
-import API from "../Services/Api";
 import JobCard from "../components/JobCard";
+import API from "../Services/Api";
 const Home = () => {
   const [jobs, setJobs] = useState([]);
   useEffect(() => {

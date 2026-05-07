204693077a09ad353675f212e66c5cf1fa3ef9eb
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-07 10:59:09 +0530

    fix imports

--- src/Services/Api.js
+++ src/Services/Api.js
@@ -1,15 +1,48 @@
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:3000/api/v1",
+//   withCredentials: true,
+// });
+
+// // Attach JWT automatically
+// API.interceptors.request.use((config) => {
+//   const token = sessionStorage.getItem("token"); // ✅ FIX
+
+//   console.log("API TOKEN:", sessionStorage.getItem("token"));
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+
+//   return config;
+// });
+
+// // Auto logout on token expire
+//  API.interceptors.response.use(
+//    (response) => response,
+//    (error) => {
+// //     if (error.response && error.response.status === 401) {
+// //       sessionStorage.clear(); // ✅ FIX
+// //       window.location.href = "/login";
+// //     }
+
+//      return Promise.reject(error);
+//    }
+//  );
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
 // Attach JWT automatically
 API.interceptors.request.use((config) => {
-  const token = sessionStorage.getItem("token"); // ✅ FIX
+  const token = sessionStorage.getItem("token");
 
-  console.log("API TOKEN:", sessionStorage.getItem("token"));
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -18,16 +51,11 @@ API.interceptors.request.use((config) => {
 });
 
 // Auto logout on token expire
- API.interceptors.response.use(
-   (response) => response,
-   (error) => {
-//     if (error.response && error.response.status === 401) {
-//       sessionStorage.clear(); // ✅ FIX
-//       window.location.href = "/login";
-//     }
-
-     return Promise.reject(error);
-   }
- );
+API.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default API;

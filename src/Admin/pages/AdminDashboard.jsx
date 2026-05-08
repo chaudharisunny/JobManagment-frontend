ec62810186e101a1bb89fc94464a39b968adb81d
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-08 12:51:27 +0530

    fix imports

--- src/Admin/pages/AdminDashboard.jsx
+++ src/Admin/pages/AdminDashboard.jsx
@@ -13,7 +13,7 @@ import {
   X,
 } from "lucide-react";
 
-import { auth } from "../../utils/auth";
+import { auth } from "@/utils/auth";
 
 const AdminDashboard = () => {
   const navigate = useNavigate();

15e1767330eb7bb27e3c17995a8b293610276b81
chaudharisunny <73020065+chaudharisunny@users.noreply.github.com>
2026-05-11 13:42:37 +0530

    fix api url

--- src/service/api.js
+++ src/service/api.js
@@ -34,6 +34,8 @@
 
 import axios from "axios";
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
+
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
